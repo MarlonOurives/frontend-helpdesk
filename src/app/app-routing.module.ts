3d274f529d2d79fce2a42129a4730b8a6437fd33
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-11-11 10:28:51 -0400

    Componente TECNICO-UPDATE implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { LoginComponent } from './components/login/login.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
@@ -12,8 +13,10 @@ const routes: Routes = [
   {
     path: '', component: NavComponent,canActivate:[AuthGuard], children:[
       {path: 'home', component: HomeComponent},
+
       {path:'tecnicos', component:TecnicoListComponent},
-      {path:'tecnicos/create', component:TecnicoCreateComponent}
+      {path:'tecnicos/create', component:TecnicoCreateComponent},
+      {path:'tecnicos/update/:id', component:TecnicoUpdateComponent}
 
     ]
   },
